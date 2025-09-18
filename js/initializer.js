5c4488a1b41ecac14006cf06be717e5b9cfdcd3d
Martim-Santos <128045103+Martim-Santos@users.noreply.github.com>
2025-09-18 07:05:54 +0100

    Update initializer.js

--- js/initializer.js
+++ js/initializer.js
@@ -1,6 +1,6 @@
 async function initPlanets() {
   try {
-    const response = await fetch('data/system2.json');
+    const response = await fetch('../SystemData.json');
     const data = await response.json();
 
     if (navigator.geolocation) {
