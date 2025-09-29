201e511ab20f762d9b5756f03d569e05124c2e1f
Martim-Santos <128045103+Martim-Santos@users.noreply.github.com>
2025-09-29 18:02:40 +0100

    Create initializerX2.js

---
+++ js/initializerX2.js
@@ -0,0 +1,23 @@
+async function initPlanets() {
+  try {
+    const response = await fetch('../data/SystemDataX2.json');
+    const data = await response.json();
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          createPlanets(position.coords.latitude, position.coords.longitude, data);
+        },
+        (error) => console.error("Erro ao obter localização:", error),
+        { enableHighAccuracy: true }
+      );
+    }
+  } catch (error) {
+    console.error("Erro ao carregar system2.json:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initPlanets();
+  updateScoreDisplay();
+});
