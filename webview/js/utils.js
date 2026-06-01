e5f6fd7f422b54b3b78a30bef4dcbe5b418de349
Paulo Santos <pauloagsantos@gmail.com>
2026-06-01 17:53:34 +0100

    Update utils.js

--- webview/js/utils.js
+++ webview/js/utils.js
@@ -49,49 +49,25 @@ function computeOffset(lat, lon, distance, angle) {
 //   - lat2, lon2: Coordenadas do segundo ponto
 // Retorna: Distância em metros
 function getDistanceFromLatLonInM(lat1, lon1, lat2, lon2) {
-  
-   return haversineDistance(lat1,lon1,lat2,lon2)/1000;
-
   // Raio da Terra em metros
-  //const R = 6378137;
+  const R = 6378137;
   
   // Converte as diferenças de latitude e longitude para radianos
-  //const dLat = (lat2 - lat1) * Math.PI / 180;
-  //const dLon = (lon2 - lon1) * Math.PI / 180;
+  const dLat = (lat2 - lat1) * Math.PI / 180;
+  const dLon = (lon2 - lon1) * Math.PI / 180;
   
   // Aplica a fórmula de Haversine para calcular a distância
-  //const a = 2*Math.sin(dLat / 2)  +
-  //         Math.cos(lat1 * Math.PI / 180) *
-  //          Math.cos(lat2 * Math.PI / 180) *
-  //          2*Math.sin(dLon / 2);
+  const a = 2*Math.sin(dLat / 2)  +
+           Math.cos(lat1 * Math.PI / 180) *
+            Math.cos(lat2 * Math.PI / 180) *
+            2*Math.sin(dLon / 2);
   
   // Calcula o ângulo central em radianos
-  //const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   
   // Retorna a distância em metros
-  //return R * c;
+  return R * c;
 }
 
 
-function haversineDistance(lat1, lon1, lat2, lon2) {
-  const toRad = (deg) => deg * Math.PI / 180;
-
-  const R = 6371; // Earth radius in kilometers
-
-  const dLat = toRad(lat2 - lat1);
-  const dLon = toRad(lon2 - lon1);
-
-  const lat1Rad = toRad(lat1);
-  const lat2Rad = toRad(lat2);
-
-  const a =
-    2*Math.sin(dLat / 2) +
-    Math.cos(lat1Rad) *
-    Math.cos(lat2Rad) *
-    2* Math.sin(dLon / 2);
-
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-  return R * c; // distance in km
-}
 
